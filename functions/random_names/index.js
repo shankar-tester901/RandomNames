247ca0deb792a407c3de4659a3f51b8ed0c54d10
Shanky <shankar.tester901@gmail.com>
2020-04-30 13:41:40 +0530

    url edited. node_modules folder removed

--- functions/random_names/index.js
+++ functions/random_names/index.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const axios = require("axios");
 const catalyst = require("zcatalyst-sdk-node");
 /**
  * This function generates a random German name and sends an email to the specified person 
@@ -7,39 +8,23 @@ const catalyst = require("zcatalyst-sdk-node");
 module.exports = (context, basicIO) => {
 	const app = catalyst.initialize(context);
 
-	let url = "https://uinames.com/api/?region=germany";
+        let url = "http://names.drycodes.com/1";
+	const response = await axios.get(url);
+	const answer = await response.data[0];
 
-
-	try {
-		request(url, function (err, response, body) {
-			if (err) {
-				basicIO.write("Error occured in fetching a random name" + err);
-				context.close();
-			} else {
-				let rand_Name = JSON.parse(body);
-				if (rand_Name == undefined) {
-					basicIO.write("Error occured in getting a random name");
+					basicIO.write(answer);
+					dispatchEmail(answer);
 					context.close();
-				} else {
-					basicIO.write(rand_Name.name + " " + rand_Name.surname + "... " + rand_Name.gender);
-					dispatchEmail(rand_Name.name, rand_Name.surname);
-					context.close();
-				}
-			}
-		});
-	} catch (err) {
-		basicIOObj.write("Error in fetching name : " + err);
-		context.close();
 	}
 
-	function dispatchEmail(firstname, lastname) {
+	function dispatchEmail(name) {
 		console.log('in dispatch email ');
 		//Create a config object with the email configuration
 		let config = {
 			from_email: 'shankarr+1002@zohocorp.com',
 			to_email: 'shankarr+1002@zohocorp.com',
-			subject: 'German Random Name Generated',
-			content: firstname + "-- " + lastname
+			subject: 'Random Name Generated',
+			content: name 
 		};
 
 		//Send the mail by passing the config object to the method which in turn returns a promise
@@ -49,4 +34,3 @@ module.exports = (context, basicIO) => {
 			console.log(mailObject);
 		});
 	}
-};
